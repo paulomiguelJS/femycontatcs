613c93add9198805220fe406e6069c8aefb48fd3
paulomigueljs <paulomiguelblues@gmail.com>
2023-03-19 21:18:01 -0400

    Button created and styled, added to NewContact page

--- src/pages/NewContact/index.jsx
+++ src/pages/NewContact/index.jsx
@@ -1,8 +1,8 @@
-import PageHeader from "../../components/PageHeader";\
+import PageHeader from "../../components/PageHeader";
 
 import Input from "../../components/Input";
 import Select from "../../components/Select";
-import <Select></Select> from "../../components/Select";
+import Button from "../../components/Button";
 
 export default function NewContact() {
   return (
@@ -15,8 +15,12 @@ export default function NewContact() {
         <option value="123">Instagram </option>
         <option value="123">Instagram </option>
       </Select>
-
-
+      <Button type="button">
+        Save Edits
+      </Button>
+      <Button type="button" disabled>
+        Save Edits
+      </Button>
     </>
   );
 }
